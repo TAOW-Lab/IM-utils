8065cc67d76a317cafc10f23f56a6e80eb365855
Khiem NV <nvkhiem2016@gmail.com>
2023-10-10 15:54:29 +0700

    feat: add constant

---
+++ src/constants/did.ts
@@ -0,0 +1,14 @@
+export const VERIFICATION_METHOD = {
+  ed25519: 'Ed25519VerificationKey2018',
+};
+
+export const MULTIBASE_PREFIX = {
+  base58btc: 'z',
+  base64: 'm',
+  base64url: 'u',
+};
+
+export const SOLANA_DID_PREFIX = {
+  devnet: 'did:solana:devnet:',
+  mainnet: 'did:solana:mainnet:',
+};
