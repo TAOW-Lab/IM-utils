92b3093d39216e29ec61b72ae15624e3b317a2dd
Khiem NV <nvkhiem2016@gmail.com>
2023-10-19 14:57:46 +0700

    chore(src/index): add global export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export const myPackage = (taco = ''): string => `${taco} from my package`;
 export * from './utils';
 export * from './methods';
 export * from './constants';
+export * from './registry';
+export * from './common';
